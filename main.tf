002ba4556294f56a61df0931988500e9eb878eff
LiyuGT <liyutenaw@gmail.com>
2023-04-12 18:49:28 -0400

    deploy resource group

--- main.tf
+++ main.tf
@@ -23,20 +23,21 @@ resource "random_integer" "deployment_id_suffix" {
 
 // Resource Group
 
-resource "azurerm_resource_group" "rg" {
-  name     = "${var.class_name}-${var.student_name}-${var.environment}-${random_integer.deployment_id_suffix.result}-rg"
+resource "azurerm_resource_group" "rg1" {
+  name     = "${var.class_name}-${var.student_name}-${var.environment}-${random_integer.deployment_id_suffix.result}-rg1"
   location = var.location
 
   tags = local.tags
 }
 
 
+
 // Storage Account
 
 resource "azurerm_storage_account" "storage" {
   name                     = "${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}st"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
+  resource_group_name      = azurerm_resource_group.rg1.name
+  location                 = azurerm_resource_group.rg1.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
