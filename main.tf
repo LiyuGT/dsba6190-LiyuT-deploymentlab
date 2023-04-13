1ae2eaa0315b16d314904e79e2d947d35271c6be
LiyuGT <liyutenaw@gmail.com>
2023-04-13 00:17:29 -0400

    deploy with machine learning

--- main.tf
+++ main.tf
@@ -44,3 +44,37 @@ resource "azurerm_storage_account" "storage" {
 
   tags = local.tags
 }
+
+// Machine Learning Workspace
+
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_application_insights" "appInsight" {
+  name                = "workspace-appInsight-ai"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  application_type    = "web"
+}
+
+resource "azurerm_key_vault" "kv" {
+  name                = "workspacekvkeyvault"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  sku_name            = "premium"
+}
+
+
+resource "azurerm_machine_learning_workspace" "mlw" {
+  name                    = "mlw-workspace"
+  location                = azurerm_resource_group.rg.location
+  resource_group_name     = azurerm_resource_group.rg.name
+  application_insights_id = azurerm_application_insights.appInsight.id
+  key_vault_id            = azurerm_key_vault.kv.id
+  storage_account_id      = azurerm_storage_account.storage.id
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
