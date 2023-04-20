7c1ba8a07da67db376a35b5eeafdd5b7dd7e0833
LiyuGT <liyutenaw@gmail.com>
2023-04-19 21:47:03 -0400

    firewall

--- main.tf
+++ main.tf
@@ -45,41 +45,41 @@ resource "azurerm_storage_account" "storage" {
   tags = local.tags
 }
 
-//Cosmos DB
-resource "azurerm_cosmosdb_account" "db" {
-  name                = "${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}db"
+
+//Firewall
+
+resource "azurerm_virtual_network" "vn" {
+  name                = "testvnet"
+  address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  offer_type          = "Standard"
-  kind                = "MongoDB"
+}
 
-  enable_automatic_failover = true
+resource "azurerm_subnet" "sub" {
+  name                 = "AzureFirewallSubnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vn.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
 
-  capabilities {
-    name = "EnableAggregationPipeline"
+resource "azurerm_public_ip" "pub" {
+  name                = "testpip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_firewall" "fire" {
+  name                = "testfirewall"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku_name            = "AZFW_VNet"
+  sku_tier            = "Standard"
+
+  ip_configuration {
+    name                 = "configuration"
+    subnet_id            = azurerm_subnet.sub.id
+    public_ip_address_id = azurerm_public_ip.pub.id
   }
-
-  capabilities {
-    name = "mongoEnableDocLevelTTL"
-  }
-
-  capabilities {
-    name = "MongoDBv3.4"
-  }
-
-  capabilities {
-    name = "EnableMongo"
-  }
-
-  consistency_policy {
-    consistency_level       = "BoundedStaleness"
-    max_interval_in_seconds = 300
-    max_staleness_prefix    = 100000
-  }
-
-  geo_location {
-    location          = "eastus"
-    failover_priority = 0
-  }
-
 }
